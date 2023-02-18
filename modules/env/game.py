a210e8dceab7daa9b4d5d3152c5d01069d387753
noguchi_daiki <bigshiny.0328@gmail.com>
2023-02-18 14:10:58 +0900

    refactor and lint fix

--- modules/env/game.py
+++ modules/env/game.py
@@ -1,11 +1,13 @@
 import os
-import sys; sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
-import numpy as np
-from conf.game_conf import BOARD_ROW, BOARD_COLUMN
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+
+import numpy as np  # noqa
+from conf.game_conf import BOARD_COLUMN, BOARD_ROW  # noqa
 
 
 class Board:
-
     def __init__(self, row: int, col: int) -> None:
         self.row = row
         self.col = col
@@ -14,20 +16,11 @@ class Board:
     def create_board(self) -> np.ndarray:
         board = np.zeros((self.row, self.col))
         return board
-    
+
     def drop_piece(self, row: int, col: int, player: int) -> None:
         self.board[row, col] = player
-    
-    def is_valid_location(self, col: int) -> None:
-        """
-        その列が既にピースで埋まっていないかをチェック
 
-        Args:
-            col (int): プレイヤーが選択した列
-        """
-        return self.board[BOARD_ROW - 1, col] == 0
-    
-    def get_next_open_row(self, col: int) -> None:
+    def get_next_open_row(self, col: int) -> int:
         """
         ボードがピースで溜まっていく
         その中で、プレイヤーがピースを入れる列を選択したときに、ピースを入れることができる行を返す
@@ -38,7 +31,25 @@ class Board:
         for r in range(BOARD_ROW):
             if self.board[r, col] == 0:
                 return r
-        
+        return -1  # 全ての行がplayer=1or2で埋まっている状態
+
+    def check_done_player(self, state: int) -> bool:
+        """
+        get_next_open_rowの返り値が-1
+        (すなわち、全ての行がplayer=1or2で埋まっているカラムをplayerが選択した状態)
+        であれば、警告を促してFalseを返す
+
+        Args:
+            col (int): プレイヤーが選択した列
+        """
+        if state == -1:
+            print(
+                "[WARNING] That column is already filled with pieces. Please select another column."
+            )
+            return False
+        else:
+            return True
+
     def print_board(self) -> None:
         print(np.flip(self.board, 0))
 
@@ -49,22 +60,19 @@ def main() -> None:
     game_over = False
     turn = 0
     while not game_over:
-        # Ask for player 1 input
         if turn % 2 == 0:
-            col = int(input("Player 1, Make your Selection(0-6):"))
-            # Player 1 will drop a piece on the board
-            if board.is_valid_location(col):
-                row = board.get_next_open_row(col)
-                board.drop_piece(row, col, 1)
-            
-        # Ask for player 2 input
+            player = 1
         else:
-            col = int(input("Player 2, Make your Selection(0-6):"))
-            # Player 2 will drop a piece on the board
-            if board.is_valid_location(col):
-                row = board.get_next_open_row(col)
-                board.drop_piece(row, col, 2)
-    
+            player = 2
+        done_playing = False
+        while not done_playing:
+            # Ask for player input
+            col = int(input(f"Player {player}, Make your Selection(0-6):"))
+            row = board.get_next_open_row(col)
+            done_playing = board.check_done_player(row)
+        # Player 1 will drop a piece on the board
+        board.drop_piece(row, col, player=player)
+
         board.print_board()
         turn += 1
 
