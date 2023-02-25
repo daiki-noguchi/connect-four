942f9af1559dcc2710739ec1a4220e0d2e27338e
noguchi_daiki <bigshiny.0328@gmail.com>
2023-02-25 13:26:15 +0900

    double dqn

--- modules/agent/dqn.py
+++ modules/agent/dqn.py
@@ -24,6 +24,7 @@ class DQNParams:
     epsilon: float = 0.1
     buffer_size: float = 10000
     batch_size: float = 32
+    double_dqn: bool = True
 
 
 class QNet(nn.Module):
@@ -67,6 +68,7 @@ class DQNAgent:
         self.buffer_size = params.buffer_size
         self.batch_size = params.batch_size
         self.action_size = params.action_size
+        self.double_dqn = params.double_dqn
 
         self.qnet = QNet(self.action_size)
         self.qnet_target = QNet(self.action_size)
@@ -88,17 +90,70 @@ class DQNAgent:
             qs = self.qnet(state)
             return int(qs.argmax().item())
 
+    def get_target(self, reward: Tensor, next_state: Tensor, done: Tensor) -> Tensor:
+        """TDターゲットを返す
+
+        Args:
+            reward (Tensor): 報酬
+            next_state (Tensor): 次のゲームボードの状態
+            done (Tensor): エピソードが終了したか
+
+        Returns:
+            Tensor: TDターゲット
+        """
+        if self.double_dqn:
+            target = self.get_ddqn_target(reward, next_state, done)
+        else:
+            target = self.get_default_target(reward, next_state, done)
+        return target
+    
+    def get_default_target(self, reward: Tensor, next_state: Tensor, done: Tensor) -> Tensor:
+        """通常のDQNのTDターゲットを返す
+
+        TDターゲット = reward + gamma * max_a qnet_target(next_state)
+
+        Args:
+            reward (Tensor): 報酬
+            next_state (Tensor): 次のゲームボードの状態
+            done (Tensor): エピソードが終了したか
+
+        Returns:
+            Tensor: TDターゲット
+        """
+        next_qs = self.qnet_target(next_state)
+        next_q = next_qs.max(1)[0]
+        next_q.detach()
+        return reward + (1 - done.to(torch.int)) * self.gamma * next_q
+    
+    def get_ddqn_target(self, reward: Tensor, next_state: Tensor, done: Tensor) -> Tensor:
+        """過大評価を解消するTDターゲットを返す
+        qnet_target(next_state)は誤差が含まれる推定値であり、この最大値を取ると、真のQ関数より過大評価されてしまう
+        Double DQNでは、真のQ関数qnet(next_state)が最大となる行動を選び、
+        その行動を取ったときのqnet_target(next_state)を使ってTDターゲットを作る
+
+        TDターゲット = reward + gamma * qnet_target(next_state, a=argmax_a qnet(next_state))
+
+        Args:
+            reward (Tensor): 報酬
+            next_state (Tensor): 次のゲームボードの状態
+            done (Tensor): エピソードが終了したか
+
+        Returns:
+            Tensor: TDターゲット
+        """
+        qs = self.qnet(next_state)
+        q_max_action = qs.argmax(axis=1)
+        next_qs = self.qnet_target(next_state)
+        next_q = torch.tensor([qi[a] for qi, a in zip(next_qs, q_max_action)])
+        next_q.detach()
+        return reward + (1 - done.to(torch.int)) * self.gamma * next_q
+
     def update(
         self, state: Tensor, action: Tensor, reward: Tensor, next_state: Tensor, done: Tensor
     ) -> None:
         qs = self.qnet(state)
         q = qs[np.arange(len(action)), action]
-
-        next_qs = self.qnet_target(next_state)
-        next_q = next_qs.max(1)[0]
-
-        next_q.detach()
-        target = reward + (1 - done.to(torch.int)) * self.gamma * next_q
+        target = self.get_target(reward, next_state, done)
 
         loss_fn = nn.MSELoss()
         loss = loss_fn(q, target)
@@ -114,3 +169,22 @@ class DQNAgent:
         state = torch.tensor(state[np.newaxis, :])
         qs = self.qnet(state)
         return int(qs.argmax().item())
+
+
+def main():
+    agent = DQNAgent(DQNParams())
+    state = torch.zeros(1, 6, 7)
+    print(state)
+    action = torch.tensor([[2]])
+    next_state = deepcopy(state)
+    print(action[0, 0])
+    next_state[0, 0, action[0, 0]] = 1
+    print(next_state)
+    
+    reward = torch.tensor([[1]])
+    done = torch.tensor([[True]])
+    agent.update(state, action, reward, next_state, done)
+
+
+if __name__ == "__main__":
+    main()
